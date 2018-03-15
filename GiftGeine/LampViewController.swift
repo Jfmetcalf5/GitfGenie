7e2e865ee01fe592b695de47d600fcd8f29713a5
Jfmetcalf5 <Jfmetcalf5@gmail.com>
2018-03-15 16:43:44 -0600

    Set up VC1 with button and genie and animations

--- GiftGeine/LampViewController.swift
+++ GiftGeine/LampViewController.swift
@@ -9,18 +9,58 @@
 import UIKit
 
 class LampViewController: UIViewController {
-
+    
     @IBOutlet weak var introTextLabel: UILabel!
     @IBOutlet weak var makeAWishButton: UIButton!
     @IBOutlet weak var genieImageView: UIImageView!
+    @IBOutlet weak var lampButton: UIButton!
+    
+    var lampShakeTimer: Timer!
+
+    var lampButtonHasBeenTapped = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        genieImageView.alpha = 0
+        makeAWishButton.alpha = 0
+        introTextLabel.alpha = 0
+        setupLampShakeTimer()
     }
     
     @IBAction func lampGenieAppearButtonTapped(_ sender: UIButton) {
+        lampButtonHasBeenTapped = true
+            
+        UIView.animate(withDuration: 0.5, animations: {
+            self.genieImageView.alpha = 1
+            self.introTextLabel.alpha = 1
+        }) { (_) in
+            
+            UIView.animate(withDuration: 0.5, delay: 3, options: .curveEaseIn, animations: {
+                self.makeAWishButton.alpha = 1
+            }, completion: nil)
+        }
+        
+        lampButton.isEnabled = false
+    }
+    
+    func addGenieAndMakeAWishButton() {
         
     }
     
-    
+    func setupLampShakeTimer() {
+        
+        lampShakeTimer = Timer.scheduledTimer(withTimeInterval: 2, repeats: true, block: { (_) in
+        
+            guard !self.lampButtonHasBeenTapped else { return }
+            
+            let animation = CABasicAnimation(keyPath: "position")
+            animation.duration = 0.1
+            animation.repeatCount = 4
+            animation.autoreverses = true
+            animation.fromValue = NSValue(cgPoint: CGPoint(x: self.lampButton.center.x - 3, y: self.lampButton.center.y))
+            animation.toValue = NSValue(cgPoint: CGPoint(x: self.lampButton.center.x + 3, y: self.lampButton.center.y))
+        
+            self.lampButton.layer.add(animation, forKey: "position")
+        })
+    }
 }
